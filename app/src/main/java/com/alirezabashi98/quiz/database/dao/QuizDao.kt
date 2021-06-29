ce560e0024f34ffa06daec31641ca3d871d6575a
alirezabashi98 <alirezabashi98@gmail.com>
2021-06-29 11:47:58 +0430

    create database

---
+++ app/src/main/java/com/alirezabashi98/quiz/database/dao/QuizDao.kt
@@ -0,0 +1,31 @@
+package com.alirezabashi98.quiz.database.dao
+
+import androidx.room.*
+import com.alirezabashi98.quiz.database.entitie.QuizModelEntity
+
+@Dao
+interface QuizDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(quiz: QuizModelEntity)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertAll(quiz: List<QuizModelEntity>)
+
+    @Query("SELECT * FROM quizModel")
+    fun getAllQuiz(): List<QuizModelEntity>
+
+    @Query("SELECT * FROM quizModel WHERE question = :question")
+    fun getQuizByTitle(question: String): List<QuizModelEntity>
+
+    @Query("SELECT * FROM quizModel WHERE id = :id")
+    fun getQuizById(id: Int): QuizModelEntity
+
+    @Delete
+    fun delete(quiz: QuizModelEntity)
+
+    @Update
+    fun updateNote(note: QuizModelEntity)
+
+
+}
