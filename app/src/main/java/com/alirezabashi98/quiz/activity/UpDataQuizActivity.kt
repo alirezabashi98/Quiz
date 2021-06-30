0454855bdd373074aa81f20713ba9a083ef98da9
alirezabashi98 <alirezabashi98@gmail.com>
2021-06-30 11:24:23 +0430

    create add & updata quiz

---
+++ app/src/main/java/com/alirezabashi98/quiz/activity/UpDataQuizActivity.kt
@@ -0,0 +1,12 @@
+package com.alirezabashi98.quiz.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.alirezabashi98.quiz.R
+
+class UpDataQuizActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_up_data_quiz)
+    }
+}
