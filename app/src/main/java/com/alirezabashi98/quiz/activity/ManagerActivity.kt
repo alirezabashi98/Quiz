e3dd4b6ebfdbd8cb90191dfcbbff92490d663b07
alirezabashi98 <alirezabashi98@gmail.com>
2021-06-30 10:58:46 +0430

    add json lottie & create view adapter & add icone item view delete & add adapter & edit manageractivity

--- app/src/main/java/com/alirezabashi98/quiz/activity/ManagerActivity.kt
+++ app/src/main/java/com/alirezabashi98/quiz/activity/ManagerActivity.kt
@@ -1,19 +1,31 @@
 package com.alirezabashi98.quiz.activity
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.airbnb.lottie.LottieAnimationView
 import com.alirezabashi98.quiz.R
+import com.alirezabashi98.quiz.adapter.ViewManagerQuizAdapter
 import com.alirezabashi98.quiz.database.dao.QuizDao
 import com.alirezabashi98.quiz.database.db.QuizDatabase
+import com.alirezabashi98.quiz.model.QuestionModel
 import com.alirezabashi98.quiz.utility.Constants
 import com.alirezabashi98.quiz.utility.ConvertTo
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import dev.shreyaspatil.MaterialDialog.MaterialDialog
 
 class ManagerActivity : AppCompatActivity() {
 
-    private lateinit var db : QuizDao
+    private lateinit var db: QuizDao
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var lottieAnimationView: LottieAnimationView
+    private lateinit var fab: FloatingActionButton
+
+    private var DataAdapter: MutableList<QuestionModel> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,19 +33,33 @@ class ManagerActivity : AppCompatActivity() {
 
         castView()
 
+        setView()
+
         addDataTest()
+
+        setDataAdapter()
+
+        setAdapter()
 
     }
 
-    private fun castView(){
+    private fun castView() {
+
+        recyclerView = findViewById(R.id.rec_managerActivity_listQuiz)
+        recyclerView.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+
+
+        lottieAnimationView = findViewById(R.id.lottie_managerActivity_nullQuiz)
+
+        fab = findViewById(R.id.fab_mangerActivity_addQuiz)
 
         db = QuizDatabase.getMyDatabase(this)!!.quizDAO()
 
     }
 
-    private fun addDataTest(){
+    private fun addDataTest() {
 
-        if (db.getAllQuiz().isNullOrEmpty()){
+        if (db.getAllQuiz().isNullOrEmpty()) {
             MaterialDialog.Builder(this)
                 .setTitle("سوالای یافت نشد")
                 .setMessage("میخوای سوالی پیش فرض اضافه کنی؟")
@@ -43,7 +69,7 @@ class ManagerActivity : AppCompatActivity() {
                 ) { dialogInterface, _ ->
                     dialogInterface.cancel()
                     db.insertAll(ConvertTo.QuizDbConvertToAllQuizDb(Constants.getQuestions()))
-                    Toast.makeText(this,"دیتاهای پیش فرض اضافه شدن",Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "دیتاهای پیش فرض اضافه شدن", Toast.LENGTH_SHORT).show()
                 }
                 .setNegativeButton(
                     "نه"
@@ -54,4 +80,35 @@ class ManagerActivity : AppCompatActivity() {
         }
 
     }
+
+    private fun setView() {
+
+        if (db.getAllQuiz().isEmpty()) {
+            recyclerView.visibility = View.GONE
+            lottieAnimationView.visibility = View.VISIBLE
+        } else {
+            recyclerView.visibility = View.VISIBLE
+            lottieAnimationView.visibility = View.GONE
+        }
+
+    }
+
+    private fun setDataAdapter() {
+
+        for (item in db.getAllQuiz()) {
+
+            DataAdapter.add(
+                ConvertTo.QuizDbConvertToQuestionModel(item)
+            )
+
+        }
+
+    }
+
+    private fun setAdapter() {
+
+        recyclerView.adapter = ViewManagerQuizAdapter(DataAdapter)
+
+    }
+
 }
